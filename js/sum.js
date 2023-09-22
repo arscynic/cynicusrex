96f997ca44540d958345cd58f35e72d344f44cae
Cynicus Rex <cynicusrex@vivaldi.net>
2023-09-22 23:25:40 +0200

    rounding

--- js/sum.js
+++ js/sum.js
@@ -2,8 +2,12 @@ document.addEventListener("DOMContentLoaded", function () {
     const terms = document.getElementsByClassName("sum");
     let sum = 0;
     for (n = 0; n < terms.length; n++) {
-      sum += parseFloat(terms[n].innerHTML);
-      terms[n].innerHTML = '&euro;' + terms[n].innerHTML;
+      const tempFloat = parseFloat(terms[n].innerHTML);
+      sum += tempFloat;
+      terms[n].innerHTML = '&euro;' + tempFloat.toFixed(2);
     }
-    total.innerHTML = sum.toFixed(2);
+    const sumMonth = sum.toFixed(2);
+    const sumYear = (sumMonth * 12).toFixed(2);
+    totalMonth.innerHTML = sumMonth;
+    totalYear.innerHTML = sumYear;
   });
